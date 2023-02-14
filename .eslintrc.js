2168425d4743f1a3b484c7a75ce381ad2af8fd3b
Karam Qaoud <karamqaoud@gmail.com>
2023-02-14 01:06:59 +0100

    Make ESLint compatiable with Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,17 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ['react'],
   rules: {
-  }
-}
+    semi: 'error',
+    preferConst: 'error',
+  },
+};
