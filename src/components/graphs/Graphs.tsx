dcb2c810c1e1158d3e86f9c109be707ea5a8f3b7
Karam Qaoud <karamqaoud@gmail.com>
2023-03-23 22:46:15 +0100

    Add graph to keep track of relations and circles to save canvas locations

--- src/components/graphs/Graphs.tsx
+++ src/components/graphs/Graphs.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react';
+
 function Graphs() {
-  const [circles, setCircles] = useState([]);
+  const [circles, setCircles] = useState(new Map());
   const [selectedCircle, setSelectedCircle] = useState(null);
   const [offsetX, setOffsetX] = useState(0);
   const [offsetY, setOffsetY] = useState(0);
   const [input1, setInput1] = useState('');
   const [input2, setInput2] = useState('');
   const canvasRef = useRef(null);
+  const graph = useRef(new Map());
+  console.log(graph);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -15,44 +18,44 @@ function Graphs() {
     ctx.lineWidth = 1;
 
     // Draw circles
-    circles.forEach((circle) => {
+    console.log('Drawing circles');
+    console.log(circles);
+    circles.forEach((circle, label) => {
+      console.log(label, circle);
       ctx.beginPath();
-      ctx.arc(circle.x1, circle.y1, circle.r1, 0, 2 * Math.PI);
+      ctx.arc(circle.x, circle.y, circle.r, 0, 2 * Math.PI);
       ctx.stroke();
       ctx.font = '14px Arial';
-      const label1Width = ctx.measureText(circle.label1).width;
-      ctx.fillText(circle.label1, circle.x1 - label1Width / 2, circle.y1 + 5);
-      ctx.beginPath();
-      ctx.arc(circle.x2, circle.y2, circle.r2, 0, 2 * Math.PI);
-      ctx.stroke();
-      const label2Width = ctx.measureText(circle.label2).width;
-      ctx.fillText(circle.label2, circle.x2 - label2Width / 2, circle.y2 + 5);
+      const label1Width = ctx.measureText(label).width;
+      ctx.fillText(label, circle.x - label1Width / 2, circle.y + 5);
     });
 
     // Draw lines between circles
     ctx.beginPath();
-    circles.forEach((circle) => {
-      const x1 = circle.x1;
-      const y1 = circle.y1;
-      const x2 = circle.x2;
-      const y2 = circle.y2;
-      const r1 = circle.r1;
-      const r2 = circle.r2;
 
-      const dx = x2 - x1;
-      const dy = y2 - y1;
-      const distance = Math.sqrt(dx * dx + dy * dy);
-      const angle = Math.atan2(dy, dx);
-      const offsetX1 = r1 * Math.cos(angle);
-      const offsetY1 = r1 * Math.sin(angle);
-      const offsetX2 = r2 * Math.cos(angle + Math.PI);
-      const offsetY2 = r2 * Math.sin(angle + Math.PI);
-      const startX = x1 + offsetX1;
-      const startY = y1 + offsetY1;
-      const endX = x2 + offsetX2;
-      const endY = y2 + offsetY2;
-      ctx.moveTo(startX, startY);
-      ctx.lineTo(endX, endY);
+    graph.current.forEach((neighbours, node) => {
+      neighbours.forEach((neighbour) => {
+        const x1 = neighbour.x;
+        const y1 = neighbour.y;
+        const x2 = node.x;
+        const y2 = node.y;
+        const r1 = neighbour.r;
+        const r2 = node.r;
+
+        const dx = x2 - x1;
+        const dy = y2 - y1;
+        const angle = Math.atan2(dy, dx);
+        const offsetX1 = r1 * Math.cos(angle);
+        const offsetY1 = r1 * Math.sin(angle);
+        const offsetX2 = r2 * Math.cos(angle + Math.PI);
+        const offsetY2 = r2 * Math.sin(angle + Math.PI);
+        const startX = x1 + offsetX1;
+        const startY = y1 + offsetY1;
+        const endX = x2 + offsetX2;
+        const endY = y2 + offsetY2;
+        ctx.moveTo(startX, startY);
+        ctx.lineTo(endX, endY);
+      });
     });
     ctx.stroke();
   }, [circles]);
@@ -101,23 +104,48 @@ function Graphs() {
     setInput2(e.target.value);
   }
 
+  function addNodeToGraph(node1, node2) {
+    if (!graph.current.has(node1)) {
+      graph.current.set(node1, []);
+    }
+
+    graph.current.get(node1).push(node2);
+  }
+
   function handleAddNodes() {
     if (input1.trim() === '' || input2.trim() === '') {
       return;
     }
-    const newCircles = [
-      ...circles,
-      {
-        x1: Math.random() * canvasRef.current.width,
-        y1: Math.random() * canvasRef.current.height,
-        r1: 20,
-        label1: input1,
-        x2: Math.random() * canvasRef.current.width,
-        y2: Math.random() * canvasRef.current.height,
-        r2: 20,
-        label2: input2,
-      },
-    ];
+
+    addNodeToGraph(input1, input2);
+    addNodeToGraph(input2, input1);
+    console.log(graph);
+    const newCircles = new Map();
+    // Copy over old circles
+    for (let [key, value] of circles) {
+      newCircles.set(key, value);
+    }
+
+    // TODO: make Circle factory class
+    const circle1 = {
+      x: Math.random() * canvasRef.current.width,
+      y: Math.random() * canvasRef.current.height,
+      r: 20,
+    };
+    const circle2 = {
+      x: Math.random() * canvasRef.current.width,
+      y: Math.random() * canvasRef.current.height,
+      r: 20,
+    };
+
+    if (!newCircles.has(input1)) {
+      newCircles.set(input1, circle1);
+    }
+
+    if (!newCircles.has(input2)) {
+      newCircles.set(input2, circle2);
+    }
+    console.log(newCircles);
     setCircles(newCircles);
     setInput1('');
     setInput2('');
@@ -125,25 +153,27 @@ function Graphs() {
 
   return (
     <div>
-      <div>
-        <label htmlFor="input1">Input 1:</label>
-        <input
-          id="input1"
-          type="text"
-          value={input1}
-          onChange={handleInput1Change}
-        />
+      <div className="inputs-for-graph">
+        <div>
+          <button onClick={handleAddNodes}>Add Nodes</button>
+          <label htmlFor="input1">Input 1:</label>
+          <input
+            id="input1"
+            type="text"
+            value={input1}
+            onChange={handleInput1Change}
+          />
+        </div>
+        <div>
+          <label htmlFor="input2">Input 2:</label>
+          <input
+            id="input2"
+            type="text"
+            value={input2}
+            onChange={handleInput2Change}
+          />
+        </div>
       </div>
-      <div>
-        <label htmlFor="input2">Input 2:</label>
-        <input
-          id="input2"
-          type="text"
-          value={input2}
-          onChange={handleInput2Change}
-        />
-      </div>
-      <button onClick={handleAddNodes}>Add Nodes</button>
       <canvas
         ref={canvasRef}
         width={500}
